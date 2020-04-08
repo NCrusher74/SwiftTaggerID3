06f2ed5df2a02fad1c6e224cc94cc2631b3ac9be
Nolaine Crusher <nolainecrusher@gmail.com>
2020-04-07 23:10:32 -0700

    Initial Commit

---
+++ SwiftTagger_MacOS/SwiftTagger_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTagger_MacOS.h
+//  SwiftTagger_MacOS
+//
+//  Created by Nolaine Crusher on 4/7/20.
+//  Copyright © 2020 Nolaine Crusher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftTagger_MacOS.
+FOUNDATION_EXPORT double SwiftTagger_MacOSVersionNumber;
+
+//! Project version string for SwiftTagger_MacOS.
+FOUNDATION_EXPORT const unsigned char SwiftTagger_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTagger_MacOS/PublicHeader.h>
+
+
