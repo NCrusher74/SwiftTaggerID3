8eb5afc3ad21ace018631e368b6d8957c1f89e83
Nolaine Crusher <nolainecrusher@gmail.com>
2020-04-08 01:48:29 -0700

    Created MacOS target

--- SwiftTagger_MacOSTests/SwiftTagger_MacOSTests.swift
+++ SwiftTagger_MacOSTests/SwiftTagger_MacOSTests.swift
@@ -7,13 +7,20 @@
 //
 
 import XCTest
+import AVFoundation
 @testable import SwiftTagger_MacOS
 
 class SwiftTagger_MacOSTests: XCTestCase {
 
+    func testChapterImport() throws {
+
+
+    }
+    
+    
     func testBasicReadAndWrite() throws {
-        XCTAssertNoThrow(try Bundle.testMp3File.read(), "error reading file")
-        XCTAssertNoThrow(try Bundle.testMp3File.write(
-            mp3File: Bundle.testMp3File, outputLocation: URL(fileURLWithPath: "/Users/nolainecrusher/Downloads/audiobook_tools/sampleaax/test/test-output.mp3")), "error writing file")
+//        XCTAssertNoThrow(try Bundle.testMp3File.read(), "error reading file")
+//        XCTAssertNoThrow(try Bundle.testMp3File.write(
+//            mp3File: Bundle.testMp3File, outputLocation: URL(fileURLWithPath: "/Users/nolainecrusher/Downloads/audiobook_tools/sampleaax/test/test-output.mp3")), "error writing file")
     }
 }
