fd64c56f7ffe04ee756d602451e8d72eae0f6457
Nolaine Crusher <nolainecrusher@gmail.com>
2020-05-19 14:01:15 -0700

    Revert "Added Workspace dependency."
    This reverts commit 9f9eefa0fdc5285095e7e865575da27510f1a038.

--- SwiftTaggerID3/Package.swift
+++ SwiftTaggerID3/Package.swift
@@ -5,20 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTaggerID3",
-    products: [.library(name: "SwiftTaggerID3", targets: ["SwiftTaggerID3"]),],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Mp3File",
+            targets: ["SwiftTaggerID3"]),
+    ],
     dependencies: [
-        .package(
-            name: "Workspace",
-            url: "https://github.com/SDGGiesbrecht/Workspace",
-            .upToNextMinor(from: Version(0, 32, 3))
-        ),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftTaggerID3",
-            dependencies: [
-                .product(name: "WorkspaceConfiguration", package: "Workspace"),
-        ]),
+            name: "Mp3File",
+            dependencies: []),
         .testTarget(
             name: "SwiftTaggerID3Tests",
             dependencies: ["SwiftTaggerID3"]),
