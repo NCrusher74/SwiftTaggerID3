062b26b3c893709a16f4e72a93e92e1ccb523544
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-05-19 18:57:48 -0600

    Disabled other platforms.

--- Workspace.swift
+++ Workspace.swift
@@ -6,6 +6,17 @@ let configuration: WorkspaceConfiguration = {
 
   let configuration = WorkspaceConfiguration()
 
+  // To my knowledge, SwiftTaggerID3 only supports macOS so far. This removes the others.
+  configuration.supportedPlatforms.subtract([
+    .windows,
+    .web,
+    .linux,
+    .tvOS,
+    .iOS,
+    .android,
+    .watchOS
+  ])
+
   // This maintains the standard “.gitignore” entries for a Swift package.
   configuration.git.manage = true
 
